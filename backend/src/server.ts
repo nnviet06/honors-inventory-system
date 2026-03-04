bbf4eb3edb22395ddf32e79c99872eee9a51c638
nnviet06 <nnviet2016@gmail.com>
2026-03-04 10:49:09 -0500

    Configure backend scripts for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import cors from 'cors';
 import equipmentRoutes from './routes';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
