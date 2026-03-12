4a08833909d5681ef471a58e55d13f513cd8eb73
nnviet06 <nnviet2016@gmail.com>
2026-03-12 15:18:25 -0400

    refactor: move fetch calls from EquipTable to equipmentService

--- frontend/src/services/equipmentService.ts
+++ frontend/src/services/equipmentService.ts
@@ -1,7 +1,39 @@
+// Refactor fetch calls to this file
+
+const BASE_URL = `${import.meta.env.VITE_API_URL}`;
+
 let token = null;
 
-const setToken = (newToken: string | null) => {
+export const setToken = (newToken: string | null) => {
     token = newToken;
 }
 
-export {setToken};
+// Refactor from EquipTable 
+export const getAllEquipment = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/equipment`);
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch equipment');
+      }
+      const data = await response.json();
+        return data;
+    } catch (err) {
+      throw err instanceof Error ? err : new Error('An error occurred while fetching equipment');
+    } 
+  };
+
+export const deleteEquipment = async (id: number) => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/equipment/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to delete equipment');
+      }
+      return response.json();
+    } catch (err) {
+        throw err instanceof Error ? err : new Error('Failed to delete equipment');
+    }
+  };
