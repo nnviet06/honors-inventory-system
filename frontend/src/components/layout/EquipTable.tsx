1461c92a8f41da50777c643d8da27fd76c3fcd19
nnviet06 <nnviet2016@gmail.com>
2026-04-10 23:01:45 -0400

    feat: added UI #19

--- frontend/src/components/layout/EquipTable.tsx
+++ frontend/src/components/layout/EquipTable.tsx
@@ -30,6 +30,9 @@ const EquipTable = ({ refreshKey, search, selectedType, selectedLocation }: Equi
     handleEdit,
     handleDelete,
     handleModalClose,
+    currentPage,
+    totalPages,
+    setCurrentPage,
   } = useEquipTable({ refreshKey, search, selectedType, selectedLocation });
 
   if (loading) {
@@ -125,6 +128,21 @@ const EquipTable = ({ refreshKey, search, selectedType, selectedLocation }: Equi
               )}
             </tbody>
           </table>
+          <div className={styles.pagination}>
+            <button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage(currentPage - 1)}
+            >
+              Previous
+            </button>
+            <span>Page {currentPage} of {totalPages}</span>
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
 
